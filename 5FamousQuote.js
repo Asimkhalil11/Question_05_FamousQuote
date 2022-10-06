b08e2fbf495a551a54966e83cfde7920512e62de
Asim Khalil <108312289+Asimkhalil11@users.noreply.github.com>
2022-10-06 10:38:21 +0500

    Add files via upload

---
+++ 5FamousQuote.js
@@ -0,0 +1,3 @@
+let famousPerson = "Muhammad SAW said"
+let message = "“When you see a person who has been given more than you in money and beauty, then look to those who have been given less.”"
+console.log(famousPerson + " " + message)
